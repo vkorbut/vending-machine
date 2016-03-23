2fd6c8f0ad079ea9c6ea72dfb660f66e21e1e992
Vladimir Korbut <v.korbut@svitla.com>
2016-03-23 17:13:58 +0200

    Interface to vending machine's hardware. Scaladoc

--- src/main/scala/com/github/vkorbut/fctryout/hardware/MachineHardwareInterface.scala
+++ src/main/scala/com/github/vkorbut/fctryout/hardware/MachineHardwareInterface.scala
@@ -1,22 +1,48 @@
 package com.github.vkorbut.fctryout.hardware
 
 /**
+ * Interface to vending machine's hardware. 
+ * 
  * @author Vladimir Korbut v.e.korbut@gmail.com
  */
 trait MachineHardwareInterface {
+  /**
+   * Defines buttons which machine does have. 
+   */
   val buttonNames: Seq[String]
 
   def logError(message: String)
 
+  /**
+   * Causes returning money to user.
+   * @param amount amount of money to be returned.
+   */
   def returnChange(amount: BigDecimal): Unit
 
+  /**
+   * Enables/disables cash acceptor making user able (or not able) 
+   * to insert money into machine
+   * @param active <code>true</code> if cash acceptor should become active, <code> false</code> otherwise. 
+   */
   def cashAcceptorMode(active: Boolean)
 
   def buttonHighlightMode(buttonName: String, highlighted: Boolean)
 
+  /**
+   * Used for enabling/disabling buttons due to invalid conditions for products.
+   * @param buttonName button to be disabled
+   * @param enabled state of the button
+   */
   def buttonEnableMode(buttonName: String, enabled: Boolean)
 
-  def prepareDrink(product: String)
+  /**
+   * Prepares and returns the product to the user.
+   * @param buttonName selected product identified by appropriate button.
+   */
+  def prepareProduct(buttonName: String)
 
+  /**
+   * Listener to hardware events
+   */
   val hwEvent: Event => Unit
 }
